8f560f0d0e3f990860c106a0fd4b4c69f3a95be5
hayashida <kenyalintian@gmail.com>
2025-06-08 19:51:58 +0900

    first commit

---
+++ src/components/Results.tsx
@@ -0,0 +1,31 @@
+// Results.tsx
+
+import { ResultsState }
+from "../App"
+
+type ResultsProps = {
+        results: ResultsState
+    }
+
+const Results = (props: ResultsProps) => {
+    return (
+        <div>         
+            {props.results.country && 
+            <div>
+                <div className="results-country">{props.results.country}</div>
+                <div className="results-city">{props.results.cityName}</div>
+                <div className="results-temp">{props.results.temperature} <span>°C</span></div>
+                <div className="results-condition">
+                    <img src={props.results.icon} alt="icon"/>
+                    <span>{props.results.conditionText}</span>
+                </div>
+        </div>
+            }
+            </div>
+
+            )
+
+
+                                        }
+
+export default Results
---
+++ src/components/Title.tsx
@@ -0,0 +1,5 @@
+// Title.tsx
+
+const Title = () => <h1>React world Weather</h1>
+
+export default Title
